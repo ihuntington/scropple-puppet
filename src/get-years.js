4b2c7e2d9cf53de53b6c599bdf611f2df29146e0
Ian Huntington <ihuntington85@gmail.com>
2020-01-03 16:51:31 +0000

    Tidy up get-years script

--- src/get-years.js
+++ src/get-years.js
@@ -1,20 +1,18 @@
 const minimist = require('minimist');
 const Scropple = require('./scropple-puppet');
-const config = require('../config.json');
 
 async function start() {
     const { year } = minimist(process.argv);
-    const scropple = new Scropple(config);
+    const scropple = new Scropple(require('../config.json'));
 
     await scropple.getYears(year);
     await scropple.exit();
-
-    process.exit(0);
 }
 
 if (module === require.main) {
-    start().catch((err) => {
-        console.log(err.stack);
-        process.exit(1);
+    start();
+
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
     });
 }
