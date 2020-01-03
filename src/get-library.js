b7e7041019ddd33ff5887dd7d0303b8f058fc02d
Ian Huntington <ihuntington85@gmail.com>
2020-01-03 15:50:58 +0000

    Update get-library script
    - export the start function
    - add error handle for unhandledRejections
    - add strict mode

--- src/get-library.js
+++ src/get-library.js
@@ -1,14 +1,20 @@
+'use strict';
+
 const Scropple = require('./scropple-puppet');
-const config = require('../config.json');
 
 async function start() {
-    const scropple = new Scropple(config);
+    const scropple = new Scropple(require('../config.json'));
+
     await scropple.getLibrary();
     await scropple.exit();
-
-    process.exit(0);
 }
 
-start().catch((err) => {
-    console.log(err.stack);
-});
+module.exports = start;
+
+if (module === require.main) {
+    start();
+
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
+    });
+}
